b325b07974741c93dee13c6d2cb89f4a405ba4da
khiemk31 <khiemk31@gmail.com>
2022-07-20 09:39:43 +0700

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,6 @@
 const {v2: cloudinary} = require('cloudinary');
 
 const port = 5000;
-
 const mysqlConfig = {
   host: 'localhost',
   user: 'modelfashion',
@@ -9,7 +8,6 @@ const mysqlConfig = {
   port: 3306,
   database: 'modelfashion',
 };
-
 cloudinary.config({
   cloud_name: 'cde',
   api_key: '537853312614449',
@@ -21,15 +19,12 @@ const messagingServiceSid = 'MG059e5c8e3ed10244b8ad968aabf85591';
 const authToken = '745e08515d9df8263764426ff49b6a63';
 
 const private_key = 'admin';
-//tk twilo
+//Twilio
 // ginhotaru282@gmail.com
 // dangtrungkien300197
-
-//khiemk31@gmail.com
-//nguyenduykhiem171098
-
-//cloudinary
-//ginhotaru282@gmail.com
+// khiemk31@gmail.com
+// nguyenduykhiem171098
+//Cloudinary
+// ginhotaru282@gmail.com
 // Matkhau123@
-
 module.exports = {port, mysqlConfig, cloudinary, private_key, accountSid, messagingServiceSid, authToken};
