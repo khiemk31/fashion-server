f92412f65fd5fa0612aac8a2233c7a2c44251aab
khiemk31 <khiemk31@gmail.com>
2022-08-08 01:45:52 +0700

    nothing

--- src/sql/productSQL.js
+++ src/sql/productSQL.js
@@ -11,11 +11,7 @@ module.exports = {
     insertListImage: `INSERT INTO product_detail_image set ?`,
     getDetailProduct: `SELECT  product_id, product_name ,price ,discount ,product_image ,product_bgr1 ,product_bgr2 ,product_bgr3    FROM  product  WHERE product.product_id = ?`,
     getListSizeDetailProduct: `select size , quantity from size where  product_id=?`,
-<<<<<<< HEAD
     queryProductByCategory: `SELECT category.category_name , category.category_id , product.product_id ,product.product_name, product.product_image , product.price , product.discount  from  product , category WHERE  product.category_id = category.category_id and  product.deleted_at is NULL and category.deleted_at is NULL and category.category_id=? ORDER BY product.discount DESC LIMIT 5`,
-=======
-    queryProductByCategory: `SELECT category.category_name , category.category_id , product.product_id ,product.product_name, product.product_image , product.price , product.discount  from  product , category WHERE  product.category_id = category.category_id and  product.deleted_at is NULL and category.deleted_at is NULL and category.category_id=? ORDER BY discount DESC LIMIT 5 `,
->>>>>>> 2cb57b148037dc948397853ac6afcd1db0ea8cff
     querySearchProductByName: `SELECT product.product_id ,product.product_name, product.product_image , product.price  from  product , category WHERE  product.category_id = category.category_id and  product.deleted_at is NULL and category.deleted_at is NULL and product.product_name LIKE % =? `,
     getAllProductByCategory: `SELECT category.category_id, category.category_name , product.product_id ,product.product_name, product.product_image , product.price , product.discount FROM product , category WHERE product.category_id = category.category_id and product.deleted_at is NULL and category.deleted_at is NULL ORDER BY category.category_id ASC , product.discount DESC`,
 };
