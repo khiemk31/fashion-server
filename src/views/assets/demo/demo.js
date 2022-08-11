b24a9c3ea1a9e62f682f40e05d7f26ef0d519a06
khiemk31 <khiemk31@gmail.com>
2022-08-12 00:17:03 +0700

    update

--- src/views/assets/demo/demo.js
+++ src/views/assets/demo/demo.js
@@ -22,7 +22,7 @@ demo = {
         var params = JSON.stringify({
             id: id,
         });
-        let url = 'http://modelfashion.store' + status;
+        let url = 'http://localhost:5000' + status;
         const res = await this.callAPI(url, params, method);
         this.showNotification('top', 'right', res.message);
         setTimeout(function () {
@@ -32,7 +32,7 @@ demo = {
     async top10User() {
         let method = 'GET';
         var params;
-        let url = 'http://modelfashion.store/top10User';
+        let url = 'http://localhost:5000/top10User';
         const res = await this.callAPI(url, params, method);
         if (res.listUser.length > 0) {
             var temp = '';
@@ -49,7 +49,7 @@ demo = {
     async top10Product() {
         let method = 'GET';
         var params;
-        let url = 'http://modelfashion.store/top10Product';
+        let url = 'http://localhost:5000/top10Product';
         const res = await this.callAPI(url, params, method);
         console.log(res.listProduct);
         if (res.listProduct.length > 0) {
@@ -73,7 +73,7 @@ demo = {
         var params = JSON.stringify({
             year: 2022,
         });
-        let url = 'http://modelfashion.store/revenue';
+        let url = 'http://localhost:5000/revenue';
         const res = await this.callAPI(url, params, method);
 
         const listRevenue = res.listRevenue;
@@ -121,7 +121,7 @@ demo = {
         ctx = document.getElementById('chartBill').getContext('2d');
         let method2 = 'GET';
         var params2;
-        let url2 = 'http://modelfashion.store/billStatistics';
+        let url2 = 'http://localhost:5000/billStatistics';
         const res2 = await this.callAPI(url2, params2, method2);
 
         myChart = new Chart(ctx, {
@@ -148,7 +148,7 @@ demo = {
         var speedCanvas = document.getElementById('speedChart');
         let method3 = 'GET';
         var params3;
-        let url3 = 'http://modelfashion.store/billDetailStatistics';
+        let url3 = 'http://localhost:5000/billDetailStatistics';
         const res3 = await this.callAPI(url3, params3, method3);
         const listBillDetailStatistics = res3.listBillDetailStatistics;
         var listBillWaiting = [];
